87547f1467d73136be8fbc3600844ed1cfef3c1b
Peter Ericson <pdericson@pdericson.com>
2017-11-27 21:51:30 +0800

    Start use case 1

--- mate/__main__.py
+++ mate/__main__.py
@@ -3,20 +3,52 @@
 Aye Aye, Captain!
 
 Usage:
-  mate
+  mate FILE...
 
 """
 
 from __future__ import print_function
 
+import json
+import logging
+import os
+import re
+import sys
+
 import docopt
+import jsonschema
+import yaml
 
 import mate
 
 
 def main():
     args = docopt.docopt(__doc__, version=mate.__version__)
-    print("Aye Aye, Captain!")
+
+    logging.basicConfig(format="%(message)s")
+
+    for path in args['FILE']:
+        with open(path) as f:
+            if re.search(r'\.ya?ml$', path, re.I):
+                resource = yaml.load(f.read())
+            else:
+                resource = json.load(f)
+
+        if resource is None or 'kind' not in resource:
+            logging.error("Unknown resource.")
+            sys.exit(1)
+
+        schema_path = os.path.join(os.path.dirname(__file__), 'data/kubernetes-json-schema/v1.8.1-local')
+
+        with open(os.path.join(schema_path, resource['kind'].lower() + 'spec.json')) as f:
+            schema = json.load(f)
+
+        resolver = jsonschema.RefResolver('file://' + schema_path + '/', None)
+        try:
+            jsonschema.validate(resource, schema, resolver=resolver)
+        except jsonschema.ValidationError as exc:
+            logging.error("{}: {}".format(path, exc))
+            sys.exit(1)
 
 
 if __name__ == '__main__':
--- setup.py
+++ setup.py
@@ -2,4 +2,5 @@ from distutils.core import setup
 
 setup(name='mate',
       packages=['mate'],
+      package_data={'mate': ['data/kubernetes-json-schema/v1.8.1-local/*.json']},
      )
